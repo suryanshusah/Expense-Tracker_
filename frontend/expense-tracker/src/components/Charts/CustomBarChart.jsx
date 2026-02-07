b7f505330fc079b12bcff785903ab298a6a59779
suryanshusah <suryanshusah56@gmail.com>
2026-02-07 20:59:20 +0530

    Made some changes in Custom bar chart

--- frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
+++ frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
@@ -21,12 +21,16 @@ const CustomBarChart=({data})=>{
 
     const InternalToolTip =({active ,payload})=>{
         if (active && payload && payload.length){
+            const item = payload[0].payload;
             return (
                <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
-                <p className="text-xs font-semibold text-purple-800 mb-1">{payload[0].payload.category}</p>
+                <p className="text-xs font-semibold text-purple-800 mb-1">
+                  {item.category || item.source || item.month}
+                </p>
                 <p className="text-sm text-gray-600">
-                    Amount :<span className="text-sm font-medium text-gray-900">
-                        ${payload[0].payload.amount}
+                    Amount :
+                    <span className="text-sm font-medium text-gray-900 ml-1">
+                        ${item.amount}
                     </span>
                 </p>
                </div>
@@ -40,25 +44,26 @@ const CustomBarChart=({data})=>{
         <ResponsiveContainer width="100%" height={300}>
             <BarChart data={data}>
                 <CartesianGrid stroke="none" />
-                    <XAxis dataKey="category" tick={{fontSize:12 ,fill:"#555"}} stroke="none"/>
-                    <YAxis tick={{fontSize:12 ,fill:"#555"}} stroke="none"/>
-                    <Tooltip content={InternalToolTip} />
+                <XAxis
+                  dataKey={(item) => item.category || item.month}
+                  tick={{fontSize:12 ,fill:"#555"}}
+                  stroke="none"
+                />
+                <YAxis tick={{fontSize:12 ,fill:"#555"}} stroke="none"/>
+                <Tooltip content={<InternalToolTip />} />
 
-                    <Bar 
-                       dataKey="amount"
-                       fill="#FF8042"
-                       radius={[10,10,0,0]}
-                       activeDot ={{r:8 ,fill:"yellow"}}
-                       activeStyle ={{fill:"green"}}
-                       >
-                        {data.map((entry,index) =>(
-                            <Cell key={index} fill={getBarColor(index)} />
-                        ))}
-                       </Bar>
+                <Bar 
+                   dataKey="amount"
+                   radius={[10,10,0,0]}
+                >
+                    {data.map((entry,index) =>(
+                        <Cell key={index} fill={getBarColor(index)} />
+                    ))}
+                </Bar>
             </BarChart>
         </ResponsiveContainer>
     </div>
   )
 }
 
-export default CustomBarChart
+export default CustomBarChart;
