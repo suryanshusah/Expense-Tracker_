01f7bbed00cb79271919b4bf713ad9e5161c3a79
suryanshusah <suryanshusah56@gmail.com>
2026-02-13 14:06:46 +0530

    Added income download details.

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -98,7 +98,28 @@ const Income = () => {
 
 
   //Handle download income detail
-  const handleDownloadIncomeDetails = async ()=>{};
+  const handleDownloadIncomeDetails = async ()=>{
+     try {
+      const response= await axiosInstance.get(API_PATHS.INCOME.DOWNLOAD_INCOME,
+        {
+          responseType:"blob"
+        }
+      );
+
+      //Create a url for blob
+      const url =window.URL.createObjectURL(new Blob([response.data]))
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download" ,"income_details.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.log("Error while downloading income data: " , error);
+      toast.error("Failed to download income details. Please try again ")
+    }
+  };
 
   useEffect(()=>{
     fetchIncomeDetails();
